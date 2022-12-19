aa758458120886ce10ffdde48355bc06de5e1105
Brayan Hernandez <brayandhf@gmail.com>
2022-12-19 18:31:30 -0300

    unit test

---
+++ src/tests/_saveQuestionAnswer.test.js
@@ -0,0 +1,24 @@
+import { _saveQuestionAnswer } from "../utils/_DATA";
+
+describe("_saveQuestionAnswer", () => {
+	it(" will verify that true is returned when correctly formatted data is passed to the function.", async () => {
+		var questionAnswer = {
+			authedUser: "brayanhernandez",
+			qid: "xj352vofupe1dqz9emx13r",
+			answer: "optionOne",
+		};
+		var result = await _saveQuestionAnswer(questionAnswer);
+		expect(result).toEqual(true);
+	});
+
+	it("will verify that an error is returned if incorrect data is passed to the function.", async () => {
+		var invalidQuestionAnswer = {
+			authedUser: "",
+			qid: "xj352vofupe1dqz9emx13r",
+			answer: "optionOne",
+		};
+		await expect(_saveQuestionAnswer(invalidQuestionAnswer)).rejects.toEqual(
+			"Please provide authedUser, qid, and answer"
+		);
+	});
+});
