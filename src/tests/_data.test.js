3c93cb0b95b2b3cd2216be4d36a46001349c0657
Brayan Hernandez <brayandhf@gmail.com>
2022-12-19 18:17:29 -0300

    unit test

---
+++ src/tests/_data.test.js
@@ -0,0 +1,26 @@
+import { _saveQuestion } from "../utils/_DATA";
+
+describe("_saveQuestion", () => {
+	it("will verify that the saved question is returned and all expected fields are populated when correctly formatted data is passed to the function", async () => {
+		var question = {
+			optionOneText: "Hola",
+			optionTwoText: "Chau",
+			author: "brayanhernandez",
+		};
+		var result = await _saveQuestion(question);
+		expect(result.author).toEqual(question.author);
+		expect(result.optionOne.text).toEqual(question.optionOneText);
+		expect(result.optionTwo.text).toEqual(question.optionTwoText);
+	});
+
+	it("will verify that an error is returned if incorrect data is passed to the function.", async () => {
+		var invalidQuestion = {
+			optionOneText: "Hola",
+			optionTwoText: "Chau",
+			author: "",
+		};
+		await expect(_saveQuestion(invalidQuestion)).rejects.toEqual(
+			"Please provide optionOneText, optionTwoText, and author"
+		);
+	});
+});
